e985c2a4134d6f4752db8f7576a94f41d634a5aa
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-01-11 02:14:07 +0900

    Add:meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<svelte:head>
+    <title>Rinrin.rs | Home</title>
+    <meta name="title" content="Rinrin.rs | Home">
+    <meta name="description" content="Rinrin.rs のホームページです(・。・)o彡ﾟ">
+
+    <meta property="og:title" content="Home">
+	<meta property="og:description" content="Rinrin.rs のホームページです(・。・)o彡ﾟ">
+    <!-- TODO: Input the link to here ↓↓  -->
+    <meta property="og:url" content="https://rinrin.pages.dev">
+    <!-- ↓ website or article -->
+    <meta property="og:type" content="website">
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
