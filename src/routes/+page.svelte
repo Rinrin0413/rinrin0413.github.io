b835c3f31d83fce58e52a990f44aee82af67cad1
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-01-11 04:51:56 +0900

    Update:_

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,30 @@
-<svelte:head>
-    <title>Rinrin.rs | Home</title>
-    <meta name="title" content="Rinrin.rs | Home">
-    <meta name="description" content="Rinrin.rs のホームページです(・。・)o彡ﾟ">
+<script>
+	let title = 'Home';
+	let desc = 'Rinrin.rs のホームページです(・。・)o彡ﾟ';
+</script>
 
-    <meta property="og:title" content="Home">
-	<meta property="og:description" content="Rinrin.rs のホームページです(・。・)o彡ﾟ">
-    <!-- TODO: Input the link to here ↓↓  -->
-    <meta property="og:url" content="https://rinrin.pages.dev">
-    <!-- ↓ website or article -->
-    <meta property="og:type" content="website">
+<svelte:head>
+	<title>Rinrin.rs | {title}</title>
+	<meta name="title" content="Rinrin.rs | {title}" />
+	<meta name="description" content={desc} />
+
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={desc} />
+	<!-- TODO: Input the link to here ↓↓  -->
+	<meta property="og:url" content="https://rinrin.pages.dev" />
+	<meta property="og:type" content="website" />
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Hi there, I'm a 16year-old
+	<a
+		href="https://nnn.ed.jp/high_school_feature/s_high_school"
+		target="_blank"
+		rel="noreferrer noopener">S high school</a
+	>
+	student from Japan; am a low spec fool ('ω' )
+</p>
+
+<style lang="scss">
+	@import './assets/stylesheets/root.scss';
+</style>
