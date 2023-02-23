c86fe0d64c872ba5ae2e55b1e6038414ca344901
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-02-24 04:52:43 +0900

    🚚 Move: stylesheets of header and footer to `assets/stylesheets/`

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -7,19 +7,5 @@
 </footer>
 
 <style lang="scss">
-	@use '/assets/stylesheets/variables/color' as *;
-	@use '/assets/stylesheets/variables/dimension' as *;
-
-	footer {
-		height: $footer-height;
-		background-color: $text-primary;
-		color: $bg-primary;
-
-		h1 {
-			font-size: 18px;
-			font-weight: 200;
-			padding: 32px 0;
-			margin: 0;
-		}
-	}
+	@use '/assets/stylesheets/footer.scss';
 </style>
