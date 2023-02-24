db6431f8b85070966353d2f11bc6cbb03b5f0801
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-02-24 14:55:30 +0900

    ✨ Add: new components `hr` and `space`

---
+++ src/lib/components/hr.svelte
@@ -0,0 +1,15 @@
+<hr />
+
+<style lang="scss">
+	@use '/assets/stylesheets/variables/color' as *;
+
+	hr {
+		height: 2px;
+		margin: 0;
+		padding: 0;
+		border: 0;
+        $border: 1.46px solid $line-primary;
+        border-top: $border;
+        border-bottom: $border;
+	}
+</style>
---
+++ src/lib/components/space.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    /** The intrinsic height of the space. */
+	export let height: string;
+</script>
+
+<div style="height: {height};" />
+
+<style>
+	div {
+		display: block;
+	}
+</style>
