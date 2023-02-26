e09d5ee1784bb6cd810752cd728430b1b27b6470
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-02-26 11:47:28 +0900

    ⚗️ Experim: comment out `transform-origin` style (#1)

--- src/lib/components/title.svelte
+++ src/lib/components/title.svelte
@@ -47,9 +47,9 @@
 
 		&#shadow {
 			transform: translateX(-50%) scale(1, -1) 
-				// perspective(128px) 
+				perspective(128px) 
 				rotate3d(-1, 0, 0, 45deg);
-			transform-origin: 50% 43px;
+			// transform-origin: 50% 43px;
 			// filter: blur(1.2px);
 			// color: transparent;
 			// background: linear-gradient(0deg, rgba($text-primary, 0.4), transparent 72%);
