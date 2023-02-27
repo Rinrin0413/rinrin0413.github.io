8851629add3e3711f5e36121e467b4dc33be42ba
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-02-28 03:03:57 +0900

    ✨ Add: new utility function `fmt_month` to format month number to month name

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -9,3 +9,9 @@ export function calc_age() {
 	age -= month < birthday.month || (month == birthday.month && date < birthday.date) ? 1 : 0;
 	return age;
 }
+
+/** Formats the given month number to a string. */
+export function fmt_month(month: number) {
+	const months = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
+	return months[month - 1];
+}
