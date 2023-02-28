5561385cb38d6ddc84d9f6328dc767aeeeb66dae
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-03-01 08:53:42 +0900

    🍱 Update: chronicle

--- src/lib/data/chronicle.ts
+++ src/lib/data/chronicle.ts
@@ -1,11 +1,40 @@
 export const chronicle = [
 	{
 		year: 2006,
-		events: [{ month: 4, event: 'この世界に湧く' }]
+		events: [{ month: 4, event: '横浜に湧く' }]
+	},
+	{
+		year: 2011,
+		events: [{ month: null, event: 'パソコン(Windows 7)を触る。この頃から rinrin と名乗ってアメーバピグとかやってた' }]
+	},
+	{
+		year: 2015,
+		events: [{ month: 6, event: 'お引越し 栃木に引っ越した。' }]
+	},
+	{
+		year: 2016,
+		events: [{ month: 12, event: '初めてマインクラフト(Wii U版)をやる' }]
+	},
+	{
+		year: 2019,
+		events: [
+			{ month: 3, event: '小学校卒業してまたお引越し 千葉に引っ越した。' },
+			{ month: 4, event: 'Windows 10版のマインクラフトを始める' },
+		]
 	},
 	{
 		year: 2020,
-		events: [{ month: 5, event: 'Discord bot を作りたくて間違って Python を始める' }]
+		events: [
+			{ month: 5, event: 'Discord bot を作りたくて間違って Python を始める' },
+			{ month: null, event: '中旬頃から壊れ始める' },
+		]
+	},
+	{
+		year: 2021,
+		events: [
+			{ month: 4, event: '春休み明けに無事崩壊し不登校になる。ここから改善することはなかった' },
+			{ month: 11, event: 'Rust を知って Python を捨てる' }
+		]
 	},
 	{
 		year: 2022,
@@ -14,6 +43,10 @@ export const chronicle = [
 			{ month: 4, event: 'S高等学校に名前を書き　入学...' },
 			{ month: 6, event: '吹っ切れてまた Windows を消し飛ばして次は openSUSE に乗り換える' }
 		]
+	},
+	{
+		year: 2023,
+		events: [{ month: 2, event: 'このサイトを SvelteKit を使って書き直す' }]
 	},
 	{
 		year: 2036,
