083653fc62904537364bddda69c8329a8730ecf6
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-03-04 07:33:23 +0900

    ✨ Add: new section "contents"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,12 +37,23 @@
 
 <Hr />
 
+<section id="contents">
+	<Title text="Table of Contents" ja="もくじー" />
+	<ul>
+		<a href="#activities"><li>Major Activities</li></a>
+		<a href="#chronicle"><li>Chronicle</li></a>
+		<a href="#contact"><li>Contact / Social</li></a>
+	</ul>
+</section>
+
+<Hr />
+
 <section id="activities">
 	<Title text="Major Activities" ja="主なすること。" />
 	<ul>
-		<li><a href="#acivity-programming"> Programming </a></li>
-		<li><a href="#acivity-game"> Game </a></li>
-		<li><a href="#acivity-music"> Music </a></li>
+		<li><a href="#acivity-programming">Programming</a></li>
+		<li><a href="#acivity-game">Game</a></li>
+		<li><a href="#acivity-music">Music</a></li>
 	</ul>
 	<Activities />
 </section>
@@ -113,6 +124,56 @@
 		&:active {
 			box-shadow: none;
 			transform: translateY(8px) scale(0.968);
+		}
+	}
+
+	#contents {
+		height: 298px;
+
+		ul {
+			list-style: none;
+			max-width: 326px;
+			padding: 0;
+			margin: 24px auto;
+			background-color: #fff6e58c;
+			border: 4px solid #b1976c8c;
+			border-radius: 4px;
+
+			a {
+				color: inherit;
+				transition: 0.2s;
+
+				&:hover {
+					color: #9c6113;
+
+					li {
+						background-color: #fff6e5b2;
+						font-size: 24px;
+					}
+				}
+
+				&:active li {
+					font-size: 20px;
+				}
+
+				li {
+					font-size: 20px;
+					font-weight: bold;
+					padding: 18px 0;
+					$border: 1px solid #b1976c;
+					border-top: $border;
+					border-bottom: $border;
+					transition: 0.24s;
+				}
+
+				&:first-child li {
+					border-top: none;
+				}
+
+				&:last-child li {
+					border-bottom: none;
+				}
+			}
 		}
 	}
 
