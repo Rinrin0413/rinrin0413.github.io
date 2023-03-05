1fd30c50edc9e10dfb647a149459aee66c4042d3
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-03-05 16:06:56 +0900

    🐛 Fix: header logo no longer fades in multiple times (fkios)

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
+
+	onMount(update_scroll);
 
 	let is_at_top = true;
 
-	if (browser) {
-		update_scroll();
+	if (browser) window.addEventListener('scroll', update_scroll);
 
-		window.addEventListener('scroll', update_scroll);
-	}
+	let enable_fade_in = true;
+
+	$: if (!is_at_top) enable_fade_in = false;
 
 	function update_scroll() {
 		is_at_top = window.scrollY <= 0;
@@ -16,7 +19,9 @@
 
 <div id="header-bg" class:blur={is_at_top} />
 <header>
-	<a href="/" id="logo" class:center={is_at_top}><img src="images/favicon.png" alt="Logo" /></a>
+	<a href="/" id="logo" class:center={is_at_top} class:fade-in={enable_fade_in}
+		><img src="images/favicon.png" alt="Logo" /></a
+	>
 </header>
 
 <style lang="scss">
