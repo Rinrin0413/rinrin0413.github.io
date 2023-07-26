e13df062e60022e231b151b8aa8f5d64e95ab663
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-07-27 08:29:34 +0900

    🌐 I18n: activities section

--- src/routes/activities.svelte
+++ src/routes/activities.svelte
@@ -23,7 +23,9 @@
 		<br />
 		<h2 class="c5">Graphics</h2>
 		<p>
-			{$_('activities.programming.graphics.desc')}<br />{$_('activities.programming.graphics.langsToUse')}<br />
+			{$_('activities.programming.graphics.desc')}<br />{$_(
+				'activities.programming.graphics.langsToUse'
+			)}<br />
 			<a href="https://learn.microsoft.com/en-us/windows/win32/direct3dhlsl/dx-graphics-hlsl"
 				>HLSL</a
 			>({$_('activities.programming.graphics.mayNotUse')}),
@@ -34,13 +36,15 @@
 		<h2 class="c5">Web Dev.</h2>
 		<p>
 			{$_('activities.programming.webdev.0')}<a href="https://sass-lang.com">Sass</a>{$_(
-				'activities.webdev.1'
+				'activities.programming.webdev.1'
 			)}<a href="https://svelte.dev/">Svelte</a>{$_('activities.programming.webdev.2')}
 		</p>
 		<br />
 		<h2 class="c5"><a href="https://git-scm.com">Git</a></h2>
 		<p>
-			{$_('activities.programming.git.0')}<a href="https://github.com">GitHub</a>{$_('activities.programming.git.1')}
+			{$_('activities.programming.git.0')}<a href="https://github.com">GitHub</a>{$_(
+				'activities.programming.git.1'
+			)}
 		</p>
 	</div>
 	<div class="act act-game" class:fade-in-up-zoom={false}>
@@ -50,36 +54,27 @@
 		<h2><a href="https://tetr.io">TETR.IO</a></h2>
 		<p>{$_('activities.game.tetrio')}</p>
 		<br />
-		<h2><a href="https://www.minecraft.net">Minecraft</a></h2>
-		<p>
-			一番長くやってる。Wii U版と統合版はもうやってなくて Java版がメイン。 Wii
-			U版やってた時代では建築していることが多かった。統合版始めてからはアドオン開発やらシェーダー開発もするようになったが、<del
-				>マイクロソフト</del
-			>による度重なる破壊活動でもう完全に萎えた。 Java版では ちまちま開発している。
-		</p>
+		<h2><a href="https://minecraft.net">Minecraft</a></h2>
+		<p>{$_('activities.game.mc.0')}<del>{$_('w.microsoft')}</del>{$_('activities.game.mc.1')}</p>
 		<br />
-		<h2><a href="https://www.thatskygame.com">Sky: Children of the Light</a></h2>
-		<p>最近は時間が取れなくてあんまりログインできていない。誘ってくれれば行くかも。</p>
+		<h2><a href="https://thatskygame.com">Sky: Children of the Light</a></h2>
+		<p>{$_('activities.game.sky')}</p>
 	</div>
 	<div class="act act-music" class:fade-in-up-zoom={false}>
 		<h1 id="acivity-music" class="c5">Music</h1>
 		<p class="subtitle">|♫|♪♬♪|♪♫:|❙♪|❙</p>
 		<br />
 		<h2>Listen</h2>
-		<p>
-			古典派とロマン派の(主にピアノの)クラシック曲が好き。特にフランツ・リストの作品をよく聴く。
-		</p>
+		<p>{$_('activities.music.listen')}</p>
 		<br />
 		<h2>Play</h2>
-		<p>
-			全く上手くないが上記のようなクラシック曲とかをピアノで弾いてる。
-			アルトリコーダーやらアコースティックギターやらも家にあるが全く触らない。
-		</p>
+		<p>{$_('activities.music.play')}</p>
 		<br />
 		<h2>Compose</h2>
 		<p>
-			クラシックっぽい曲を作ったりクラシックを編曲したり。 ただし理論をほとんど考えていないので
-			コード進行とかについては何も知らない。 作曲には基本的に MuseScore 3, 4 を使っている。
+			{$_('activities.music.compose.0')}<a href="https://musescore.org">MuseScore 3, 4</a>{$_(
+				'activities.music.compose.1'
+			)}
 		</p>
 	</div>
 </div>
