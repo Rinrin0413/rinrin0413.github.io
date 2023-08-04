fdf25d146763ed705a13f3c4ee0605c713d0d90d
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-08-05 00:02:58 +0900

    🚧 Wip: hamburger button toggle and drawer menu

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -5,6 +5,8 @@
 
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
+	import { toggle_scroll_prevention } from '$lib/util';
+	import { blur } from 'svelte/transition';
 	import { _ } from 'svelte-i18n';
 
 	onMount(update_scroll);
@@ -13,6 +15,7 @@
 
 	if (browser) window.addEventListener('scroll', update_scroll);
 
+	let is_drawer_menu_opened = false;
 	let enable_fade_in = true;
 
 	$: if (!is_at_top) enable_fade_in = false;
@@ -47,11 +50,20 @@
 	function update_scroll() {
 		is_at_top = window.scrollY <= 0;
 	}
+
+	/** Toggles drawer menu open/close. */
+	function toggle_drawer_menu(open: boolean) {
+		is_drawer_menu_opened = open;
+		toggle_scroll_prevention(is_drawer_menu_opened);
+	}
 </script>
 
+{#if is_drawer_menu_opened}
+	<div id="drawer-menu-bg" transition:blur={{ duration: 300 }} on:click={() => {toggle_drawer_menu(false);}} on:keypress={()=>{}} />
+{/if}
 <div id="header-bg" class:blur={is_at_top} />
 <header>
-	<a href="/" id="logo" class:center={is_at_top} class:fade-in={enable_fade_in}
+	<a href="/" id="logo" class:center={is_at_top} class:fade-in={enable_fade_in} on:click={() => {toggle_drawer_menu(false);}}
 		><img src="/images/favicon.png" alt={$_('header.logo')} /></a
 	>
 	<nav>
@@ -61,7 +73,7 @@
 				<span>{$_(item.name)}</span>
 			</a>
 		{/each}
-		<div class:at-top={is_at_top}><HamburgerButton /><LangButton /></div>
+		<div class:at-top={is_at_top}><HamburgerButton is_opened={is_drawer_menu_opened} on:toggle={(e) => {toggle_drawer_menu(e.detail.is_opened);}} /><LangButton /></div>
 	</nav>
 </header>
 
