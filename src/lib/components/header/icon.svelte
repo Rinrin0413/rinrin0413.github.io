69f1f18135340ca7f42f593af56c979d8df9dfa7
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-09-01 12:26:35 +0900

    🐸 Change `#projects` to `.projects`

--- src/lib/components/header/icon.svelte
+++ src/lib/components/header/icon.svelte
@@ -55,9 +55,8 @@
 		xmlns="http://www.w3.org/2000/svg"
 		width="16"
 		height="16"
-		class="bi bi-window"
+		class="bi bi-window projects"
 		viewBox="0 0 16 16"
-		id="projects"
 	>
 		<path
 			d="M2.5 4a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm1 .5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"
@@ -126,7 +125,7 @@
 		}
 	}
 
-	#projects {
+	.projects {
 		transform: scale(0.87);
 	}
 </style>
