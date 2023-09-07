be68f4fff89afd22701d7662cb43607d26ad32c2
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-09-08 04:45:24 +0900

    ✏️ Rename: go-to-top button -> scroll-to-home button

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,7 +4,7 @@
 
 <footer>
 	<p class="copyright">{COPYRIGHT}</p>
-	<a href="/" class="go-to-top-btn">
+	<a href="/" class="go-to-home-btn">
 		<!-- 
 			Bootstrap Icons - House door fill
 			https://icons.getbootstrap.com/icons/house-door-fill/
@@ -24,7 +24,7 @@
 				d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"
 			/>
 		</svg>
-		<span class="top-text">TOP</span>
+		<span class="home-text">HOME</span>
 	</a>
 </footer>
 
