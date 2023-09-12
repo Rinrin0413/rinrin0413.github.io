cdae841fbdec89f33efc30a9c71a64575f20ae4c
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-09-12 10:32:22 +0900

    ✨ Add: write profile page [#8]
    resolve #7
    
    - ✨ Add: write profile page
    - ✨ Impl: property `id` to `Title` component
    - ✨ Add: glassmorphism.scss

---
+++ src/routes/profile/Details.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+	import { calcAge } from '$lib/util';
+</script>
+
+<div>
+	<table class="glassmorphism">
+		<tr>
+			<td>{$_('w.name')}</td>
+			<td>Rinrin.rs</td>
+		</tr>
+		<tr>
+			<td>{$_('w.age')}</td>
+			<td>{calcAge()}{$_('w.yearsOld')}</td>
+		</tr>
+		<tr>
+			<td>{$_('w.birthday')}</td>
+			<td>{$_('profile.birthday')}</td>
+		</tr>
+		<tr>
+			<td>{$_('profile.school')}</td>
+			<td>{$_('w.sHighSchool')}</td>
+		</tr>
+		<tr>
+			<td>{$_('profile.birthplace')}</td>
+			<td>{$_('w.yokohama')}</td>
+		</tr>
+	</table>
+</div>
+
+<style lang="scss">
+	@use '/assets/stylesheets/utils/glassmorphism';
+	@use '/assets/stylesheets/variables/mixin' as *;
+	@use '/assets/stylesheets/variables/color' as *;
+
+	div {
+		max-width: 420px;
+		padding: 0 12px;
+		margin: 28px auto;
+	}
+
+	table {
+		padding: 12px;
+		margin: 0 auto;
+	}
+
+	td {
+		text-align: left;
+		font-size: 18px;
+		line-height: 1.4;
+		padding: 0 4px;
+
+		@include sp {
+			font-size: 16px;
+		}
+
+		&:first-child {
+			padding-right: 0;
+			text-align: right;
+
+			&::after {
+				content: ':';
+			}
+		}
+	}
+</style>
---
+++ src/routes/profile/Environment.svelte
@@ -0,0 +1,139 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+</script>
+
+<h2>{$_('w.hardware')}</h2>
+
+<div>
+	<h3>{$_('w.model')}</h3>
+	<p>MouseComputer Z7-Z690</p>
+
+	<h3>CPU</h3>
+	<p>Intel® Core™ i7-12700 Processor</p>
+
+	<h3>{$_('w.memory')}</h3>
+	<p>16GB✕2</p>
+
+	<h3>{$_('w.gpu')}</h3>
+	<p>NVIDIA GeForce RTX 3060 / 12GB</p>
+</div>
+
+<h2>{$_('w.software')}</h2>
+
+<div>
+	<h3>OS</h3>
+	<p>openSUSE Leap 15.4 x86_64</p>
+
+	<h3>{$_('w.shell')}</h3>
+	<p>bash 4.4.23</p>
+
+	<h3>{$_('w.desktopEnv')}</h3>
+	<p>GNOME 41.4</p>
+
+	<h3>{$_('w.webBrowser')}</h3>
+	<p>Mozilla Firefox</p>
+	<span>
+		<b>{$_('w.transition')}</b><br />
+		[IE9 -> Chrome -> Edge -> Brave -> Firefox]
+	</span>
+
+	<h3>{$_('w.codeEditor')}</h3>
+	<p>Visual Studio Code Insiders</p>
+	<span>
+		<b>{$_('w.transition')}</b><br />
+		[{$_('w.sakuraEditor')} -> Notepad++ -> VSCode -> VSCode Insiders]
+	</span>
+</div>
+
+<p>
+	{$_('env.notes.0')}<a href="https://geartics.com/rinrin" target="_blank" rel="noopener noreferrer"
+		>Geartics</a
+	>{$_('env.notes.1')}
+</p>
+
+<style lang="scss">
+	@use '/assets/stylesheets/variables/color' as *;
+	@use '/assets/stylesheets/variables/mixin' as *;
+
+	h2 {
+		display: inline-block;
+		text-align: center;
+		font-size: 28px;
+		font-weight: bold;
+		border-bottom: 6px solid rgba($text-primary, 0.5);
+		padding: 0 10px;
+
+		@include sp {
+			font-size: 24px;
+		}
+	}
+
+	h3,
+	div p {
+		max-width: 365px;
+		padding: 4px 14px;
+
+		@include sp {
+			max-width: 291px;
+		}
+	}
+
+	$glassmorphism-border-light: 2px solid #e7dfd57a;
+	$glassmorphism-border-dark: 2px solid rgba($text-primary, 0.1);
+
+	h3 {
+		margin: 0 auto;
+		font-size: 20px;
+		padding: 0 14px;
+		border-bottom: 2px solid rgba($text-primary, 0.75);
+		background-color: rgba($text-primary, 0.16);
+		$blur: blur(4px);
+		backdrop-filter: $blur;
+		-webkit-backdrop-filter: $blur;
+		border-right: $glassmorphism-border-dark;
+		border-left: $glassmorphism-border-light;
+		border-top: $glassmorphism-border-light;
+
+		@include sp {
+			font-size: 18px;
+		}
+	}
+
+	div p {
+		padding-bottom: 8px;
+		margin-top: 0;
+		margin-bottom: 14px;
+		background-color: rgba($text-primary, 0.08);
+		$blur: blur(24px);
+		backdrop-filter: $blur;
+		-webkit-backdrop-filter: $blur;
+		$border-rad: 4px;
+		border-bottom-left-radius: $border-rad;
+		border-bottom-right-radius: $border-rad;
+		border-right: $glassmorphism-border-dark;
+		border-bottom: $glassmorphism-border-dark;
+		border-left: $glassmorphism-border-light;
+	}
+
+	span {
+		font-size: 16px;
+		margin-top: 0px;
+		position: relative;
+		top: -19px;
+
+		@include sp {
+			font-size: 14px;
+		}
+	}
+
+	b {
+		font-size: 18px;
+
+		&:before {
+			content: '┗ ';
+		}
+		&:after {
+			content: ':';
+		}
+	}
+</style>
