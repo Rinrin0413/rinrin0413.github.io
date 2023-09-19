acdd930e679038a8336368280badc484b78f016d
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-09-19 10:32:21 +0900

    ✨ Add: thumbnail image to article page [#12]

--- src/routes/blog/articles/[slug]/+page.svelte
+++ src/routes/blog/articles/[slug]/+page.svelte
@@ -28,4 +28,45 @@
 	<meta property="og:type" content="article" />
 </svelte:head>
 
+<img src="/images/blog/{slug}.png" alt="Article thumbnail" id="back-thumbnail" aria-hidden="true" />
+
+<div id="thumbnail-wrapper">
+	<img src="/images/blog/{slug}.png" alt="Article thumbnail" id="thumbnail" />
+</div>
+
 <Article body={data.component} />
+
+<style lang="scss">
+	@use '/assets/stylesheets/variables/dimension' as *;
+
+	$back-thumbnail-height: 70vh;
+
+	#back-thumbnail {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: $back-thumbnail-height;
+		object-fit: cover;
+		opacity: 0.6;
+		filter: blur(4px);
+	}
+
+	#thumbnail-wrapper {
+		width: 100vw;
+		height: $back-thumbnail-height;
+		position: relative;
+		bottom: $header-height * 1.3;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	#thumbnail {
+		position: relative;
+		width: 95vmin;
+		height: 427px;
+		object-fit: contain;
+		filter: drop-shadow(0 0 14px #00000080);
+	}
+</style>
