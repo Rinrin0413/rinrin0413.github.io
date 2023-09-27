5322efe6637afa7b019a060746f5d623599b6b32
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-09-28 00:24:10 +0900

    🎨 Style: format code

--- src/routes/blog/articles/[slug]/+page.ts
+++ src/routes/blog/articles/[slug]/+page.ts
@@ -3,11 +3,15 @@ import type { SvelteComponent } from 'svelte';
 import type ArticleMetadata from '$lib/types/ArticleMetadata';
 import { error } from '@sveltejs/kit';
 
-export const load: PageLoad = async ({ params }): Promise<{
-	component: typeof SvelteComponent,
-	frontmatter: ArticleMetadata
+export const load: PageLoad = async ({
+	params
+}): Promise<{
+	component: typeof SvelteComponent;
+	frontmatter: ArticleMetadata;
 }> => {
-	const { default: component, metadata: frontmatter } = await import(`../${params.slug}.md`).catch(err);
+	const { default: component, metadata: frontmatter } = await import(`../${params.slug}.md`).catch(
+		err
+	);
 	if (!frontmatter.published) err();
 	return { component, frontmatter };
 };
