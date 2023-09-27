3b92a9dfd8757097488f8d9d32e6976a4c08ba2d
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-09-28 00:17:32 +0900

    ✨♻️ use type for article metadata [#12]
    - ✨ Add: `ArticleMetadata` type
    - ♻️ Improve: specify return type of function `load`
    - ♻️ Improve: don't use try/catch because this causes error `ts(2366)`

---
+++ src/lib/types/ArticleMetadata.ts
@@ -0,0 +1,11 @@
+// I explicitly specified ` | null` instead of optional variable.
+// This is to prevent missing fields in the article frontmatter definitions.
+export default interface ArticleMetadata {
+    published: boolean;
+    indexed: boolean;
+    title: string;
+    desc: string | null;
+    tags: string[] | null;
+    hasThumbnail: boolean;
+    imgFmt: string | null;
+};
--- src/routes/blog/articles/[slug]/+page.ts
+++ src/routes/blog/articles/[slug]/+page.ts
@@ -1,14 +1,15 @@
 import type { PageLoad } from './$types';
+import type { SvelteComponent } from 'svelte';
+import type ArticleMetadata from '$lib/types/ArticleMetadata';
 import { error } from '@sveltejs/kit';
 
-export const load: PageLoad = async ({ params }) => {
-	try {
-		const { default: component, metadata: frontmatter } = await import(`../${params.slug}.md`);
-		if (!frontmatter.published) err();
-		return { component, frontmatter };
-	} catch {
-		err();
-	}
+export const load: PageLoad = async ({ params }): Promise<{
+	component: typeof SvelteComponent,
+	frontmatter: ArticleMetadata
+}> => {
+	const { default: component, metadata: frontmatter } = await import(`../${params.slug}.md`).catch(err);
+	if (!frontmatter.published) err();
+	return { component, frontmatter };
 };
 
 function err() {
