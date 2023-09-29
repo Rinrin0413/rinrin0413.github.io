f87c592c0df1fd7e3ba18b720c010e4f89236cd5
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-09-29 17:55:28 +0900

    🎨 Style: format code

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,4 +1,4 @@
-import type ArticleMetadata from "$lib/types/ArticleMetadata";
+import type ArticleMetadata from '$lib/types/ArticleMetadata';
 
 /**
  * Adds a specified class to specified elements when they are scrolled into view.
@@ -124,27 +124,28 @@ export function idToDate(articleId: string) {
 
 /** Fetches and sorts all articles. */
 export async function fetchArticles() {
-    // Fetch all articles.
-    const articles = await Promise.all(
-        Object.entries(import.meta.glob('/src/routes/blog/articles/*.md'))
-            .map(async ([path, importArticle]) => {
-                const { metadata } = await importArticle() as { metadata: ArticleMetadata };
-                return {
-                    metadata,
-                    slug: path.split('/').pop()!.split('.')[0] // eslint-disable-line @typescript-eslint/no-non-null-assertion
-                };
-            })
-    );
+	// Fetch all articles.
+	const articles = await Promise.all(
+		Object.entries(import.meta.glob('/src/routes/blog/articles/*.md')).map(
+			async ([path, importArticle]) => {
+				const { metadata } = (await importArticle()) as { metadata: ArticleMetadata };
+				return {
+					metadata,
+					slug: path.split('/').pop()!.split('.')[0] // eslint-disable-line @typescript-eslint/no-non-null-assertion
+				};
+			}
+		)
+	);
 
-    // Sort by newest.
-    articles.sort((a, b) => calcOrder(b.slug) - calcOrder(a.slug));
+	// Sort by newest.
+	articles.sort((a, b) => calcOrder(b.slug) - calcOrder(a.slug));
 
-    return articles;
-};
+	return articles;
+}
 
 function calcOrder(slug: string) {
-    let n = parseInt(slug.split('_')[0]);
-    // It is alignment for slugs without numbering.
-    n *= n < 100000000 ? 100 : 1;
-    return n;
+	let n = parseInt(slug.split('_')[0]);
+	// It is alignment for slugs without numbering.
+	n *= n < 100000000 ? 100 : 1;
+	return n;
 }
