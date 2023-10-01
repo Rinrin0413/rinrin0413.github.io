0de4cc3151167ad3005b77296dbcd11aec24a308
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-10-01 15:24:38 +0900

    🚧 Wip: implement blog index page (base) [#15]

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -3,7 +3,11 @@
 	import Space from '$lib/components/Space.svelte';
 
 	import { SITE_URL } from '$lib/variables';
+	import { idToDate } from '$lib/util.js';
+	import { date as date_i18n } from 'svelte-i18n';
 	//import { _ } from 'svelte-i18n';
+
+	export let data;
 
 	const HEAD = {
 		title: 'Blog',
@@ -26,7 +30,28 @@
 	<Space height="64px" />
 
 	<Title text="Blog" />
-	<p>Coming soon...</p>
+
+	<ul>
+		{#each data.articles as article}
+			{@const slug = article.slug}
+			{@const meta = article.metadata}
+			{@const date = idToDate(slug)}
+
+			<li><a href={'/blog/articles/' + slug}>
+				{#if meta.hasThumbnail}
+					<img src={`/images/blog/${slug}.${meta.imgFmt}`} alt="Article thumbnail" />
+				{/if}
+				<h2>{meta.title}</h2>
+				<time datetime={date.toISOString()}>
+					{$date_i18n(date, { format: 'medium' })}
+				</time>
+				{#if meta.desc}
+					<p>{meta.desc}</p>
+				{/if}
+				<span>read more -&gt;</span>
+			</a></li>
+		{/each}
+	</ul>
 </section>
 
 <style lang="scss"></style>
---
+++ src/routes/blog/+page.ts
@@ -0,0 +1,14 @@
+import type { PageLoad } from './$types';
+import type ArticleMetadata from '$lib/types/ArticleMetadata';
+
+export const load: PageLoad = async ({
+	url,
+	fetch
+}): Promise<{
+	articles: { metadata: ArticleMetadata; slug: string }[];
+}> => {
+	const response = await fetch('/api/articles?indexed=true&' + url.searchParams);
+	console.log('/api/articles?indexed=true&' + url.searchParams);
+	const articles = await response.json();
+	return { articles };
+};
