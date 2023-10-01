cd211caa240ac2b47c5d07116ab33ba7670e8066
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-10-02 03:14:12 +0900

    ⚰️ Remove: temporary `console.log`

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -3,7 +3,6 @@ import type ArticleMetadata from '$lib/types/ArticleMetadata';
 
 export const load: PageLoad = async ({ url, fetch }): Promise<{ articles: ArticleMetadata[] }> => {
 	const response = await fetch('/api/articles?indexed=true&' + url.searchParams);
-	console.log('/api/articles?indexed=true&' + url.searchParams);
 	const articles = await response.json();
 	return { articles };
 };
