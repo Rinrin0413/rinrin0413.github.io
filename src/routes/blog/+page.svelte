4178a4ca3816c4d22b0239c751432b03724b3fc5
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-10-05 23:15:08 +0900

    🚚♻️ Move: article cards to their own component (ArticleCard) [#15]

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -2,15 +2,14 @@
 	import Space from '$lib/components/Space.svelte';
 	import Title from '$lib/components/Title.svelte';
 	import TagPicker from './TagPicker.svelte';
+	import ArticleCard from './ArticleCard.svelte';
 
 	import type { PageData } from './$types';
 	import { SITE_URL } from '$lib/variables';
-	import { idToDate } from '$lib/util.js';
 	import { fly } from 'svelte/transition';
 	import { elasticBackOut } from '$lib/easing';
 	import { expoIn, expoOut } from 'svelte/easing';
 	import { flip } from 'svelte/animate';
-	import { date as date_i18n } from 'svelte-i18n';
 	//import { _ } from 'svelte-i18n';
 
 	export let data: PageData;
@@ -44,42 +43,15 @@
 	<TagPicker allTags={data.allTags} pickedTags={tags} />
 
 	{#if 0 < articles.length}
-		<ul>
-			{#each articles as meta (meta.slug)}
-				{@const slug = meta.slug ?? 'unreachable'}
-				{@const date = idToDate(slug)}
-				{@const hasThumbnail = meta.hasThumbnail}
-
-				<li
-					in:fly={{ x: -512, duration: 1000, easing: elasticBackOut }}
-					out:fly={{ x: 256, duration: 200, easing: expoIn }}
-					animate:flip={{ duration: 700, easing: expoOut }}
-				>
-					<a href={'/blog/articles/' + slug}
-						><article>
-							<div class="thumbnail">
-								<img
-									src={'/images/' +
-										(hasThumbnail ? `blog/${slug}.${meta.imgFmt}` : 'no-image_oxipng.png')}
-									alt={hasThumbnail ? 'Article thumbnail' : 'No image'}
-									loading="lazy"
-									class:no-image={!hasThumbnail}
-								/>
-							</div>
-							<div class="meta">
-								<h2>{meta.title}</h2>
-								<time datetime={date.toISOString()}>
-									{$date_i18n(date, { format: 'medium' })}
-								</time>
-								{#if meta.desc}
-									<p>{meta.desc}</p>
-								{/if}
-							</div>
-						</article></a
-					>
-				</li>
-			{/each}
-		</ul>
+		<ul>{#each articles as meta (meta.slug)}
+			<li
+				in:fly={{ x: -512, duration: 1000, easing: elasticBackOut }}
+				out:fly={{ x: 256, duration: 200, easing: expoIn }}
+				animate:flip={{ duration: 700, easing: expoOut }}
+			>
+				<ArticleCard meta={meta} />
+			</li>
+		{/each}</ul>
 	{:else}
 		<h2>No matching articles found.</h2>
 		<a href="/blog" class="clear-btn">Clear filters</a>
