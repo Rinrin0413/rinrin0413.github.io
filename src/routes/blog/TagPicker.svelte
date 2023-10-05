871ca6ee74fee1c13ca250fa2b2d628dd8d18b18
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-10-05 22:17:17 +0900

    ✨ Add: tag picker animation [#15]

--- src/routes/blog/TagPicker.svelte
+++ src/routes/blog/TagPicker.svelte
@@ -3,34 +3,38 @@
 	import Tag from './Tag.svelte';
 
 	import type ArticleTagWithCount from '$lib/types/ArticleTagWithCount';
+	import { fly, scale } from 'svelte/transition';
+	import { bounceOut } from 'svelte/easing';
 
 	export let allTags: ArticleTagWithCount[];
 	export let pickedTags: string[];
+
+	let isOpened = true;
 </script>
 
-<details open>
-	<summary><Title2 text="Tags" /></summary>
+<button on:click={() => isOpened = !isOpened}>
+	<span class:opened={isOpened}><Title2 text="Tags" /></span>
+</button>
 
-	<ul>
-		{#each allTags as { tag, count }}
-			<Tag name={tag} {count} isEnabled={pickedTags.includes(tag)} />
-		{/each}
-	</ul>
-</details>
+{#if isOpened}<ul>{#each allTags as { tag, count }, i}
+	<div
+		in:fly={{ y: -18, duration: 400, delay: i * 52, easing: bounceOut }}
+		out:scale={{ duration: 200, delay: (allTags.length -i) * 16 }}
+	>
+		<Tag name={tag} {count} isEnabled={pickedTags.includes(tag)} />
+	</div>
+{/each}</ul>{/if}
 
 <style lang="scss">
-	details[open] summary::before {
-		rotate: 0deg;
+	button {
+		font-family: inherit;
+		color: inherit;
+		background: none;
+		border: none;
+		cursor: pointer;
 	}
 
-	summary {
-		list-style: none;
-		cursor: pointer;
-
-		&::-webkit-details-marker {
-			display: none;
-		}
-
+	span {
 		&::before {
 			content: '▼';
 			display: inline-block;
@@ -38,11 +42,18 @@
 			rotate: -90deg;
 			transition: 0.2s;
 		}
+	
+		&.opened::before {
+			rotate: 0deg;
+		}
 	}
-
 	ul {
 		padding: 0;
 		margin: 6px 0;
 		margin-bottom: 32px;
+	}
+
+	div {
+		display: inline-block;
 	}
 </style>
