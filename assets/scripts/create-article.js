668d95342f3703d3723e79e16fdefb4733dec1ee
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-10-12 05:02:26 +0900

    ✨ Add: command `npm run article` to create new article [#12]

---
+++ assets/scripts/create-article.js
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import path from 'path';
+
+const articlesDir = path.join(process.cwd(), 'articles');
+
+const now = new Date();
+const date = now.getFullYear() 
+    + String(now.getMonth() + 1).padStart(2, '0') 
+    + String(now.getDate()).padStart(2, '0');
+
+let i = 1;
+while (fs.existsSync(path.join(
+    articlesDir,
+    date + (1 < i ? String(i).padStart(2, '0') : '') + '.md'
+))) i++;
+const fileName = date + (1 < i ? String(i).padStart(2, '0') : '') + '.md';
+
+const filePath = path.join(articlesDir, fileName);
+
+const initContent = `---
+published: true
+indexed: true
+title: "ここにタイトル"
+desc: "ここに説明文"
+tags: ["タグ", "タグ"]
+hasThumbnail: false
+imgFmt: null
+---
+
+ここに本文
+
+日付を変えたかったら[記事のファイル名のルール(ファイル)](/docs/blog.md#記事のファイル名のルール)に従った上でファイル名を変更する
+
+その他の詳細についてはテスト記事参照:
+
+- Markdownファイル: [/articles/20230917_test.md](/articles/20230917_test.md)
+- ページ: [/blog/articles/20230917_test](http://127.0.0.1:5173/blog/articles/20230917_test) (vite devサーバー起動中のみ)
+`;
+
+fs.writeFileSync(filePath, initContent);
+
+console.log('Created: ' + filePath);
