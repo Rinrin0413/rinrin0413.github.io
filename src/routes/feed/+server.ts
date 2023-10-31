7cbe1c29639209618d7650dee846d610d6173fae
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-10-31 17:03:14 +0900

    🛠️ Improve: differentiate date of articles of same day in seconds [#19]

--- src/routes/feed/+server.ts
+++ src/routes/feed/+server.ts
@@ -19,14 +19,38 @@ async function body() {
 
 	const categories = renderCategories((await fetchTags()).map((t) => t.tag)).join('\n    ');
 
+    let prevDate: {
+        date: Date;
+        index: number;
+    };
 	const articles = (await fetchArticles({ only_indexed: true }))
+        .reverse()
 		.map((article) => {
 			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 			const slug = article.slug!;
+
+            let date = idToDate(slug);
+            const isFirstArticleOfTheDay = 
+                // current article IS FIRST OF ALL
+                prevDate === undefined ||
+                // OR previous article date AND current article date ARE NOT EQUAL
+                // ＊ Hour, Minute, Second, Millisecond are guaranteed to be 0
+                !(
+                    prevDate.date.getFullYear() === date.getFullYear() &&
+                    prevDate.date.getMonth() === date.getMonth() &&
+                    prevDate.date.getDate() === date.getDate()
+                );
+            if (isFirstArticleOfTheDay) {
+                prevDate = { date, index: 0 };
+            } else {
+                prevDate.index++;
+                prevDate.date.setSeconds(prevDate.index);
+                date = prevDate.date;
+            }
 			return `<entry>
         <id>urn:uuid:${uuidv5(slug, FEED_UUID)}</id>
         <title>${article.title}</title>
-        <updated>${idToDate(slug).toISOString()}</updated>
+        <updated>${(date).toISOString()}</updated>
         <link rel="alternate" href="${SITE_URL}/blog/articles/${article.slug}" />
         <summary>${article.desc}</summary>${renderCategories(article.tags ?? []).join('\n        ')}
     </entry>`;
