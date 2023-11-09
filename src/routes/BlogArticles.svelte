e79bacb4b04ea01fb01bb32ea775ff4d4c585457
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-11-10 04:08:19 +0900

    ✨ Add: intro animation for blog articles section [#23]

--- src/routes/BlogArticles.svelte
+++ src/routes/BlogArticles.svelte
@@ -2,17 +2,31 @@
 	import ArticleCard from '$lib/components/ArticleCard.svelte';
 
 	import type { ArticleMetadata } from '$lib/types';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+	import { addClassOnVisible } from '$lib/util';
 
 	export let articles: ArticleMetadata[];
+
+	onMount(fadeIn);
+	if (browser) window.addEventListener('scroll', fadeIn);
+
+	function fadeIn() {
+		addClassOnVisible(document.querySelectorAll('.article-card-container'), 'fade-in-up-zoom');
+	}
 </script>
 
 <ul>
 	{#each articles as meta}
-		<li><ArticleCard {meta} mini /></li>
+		<li class="article-card-container" class:fade-in-up-zoom={false}>
+			<ArticleCard {meta} mini />
+		</li>
 	{/each}
 </ul>
 
 <style lang="scss">
+	@use '/assets/stylesheets/utils/fade_in_up_zoom';
+
 	ul {
 		max-width: 748px;
 		padding: 0;
