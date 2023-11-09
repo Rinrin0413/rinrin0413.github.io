901c13232a744d733af25084fdb4bd4d63bf52e9
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-11-10 02:46:30 +0900

    ⚰️ Remove: unused property `thumbnailImgFmts` from `PageData` of top page [#23]

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,13 +1,9 @@
 import type { PageLoad } from './$types';
-import type { ArticleMetadata, ArticleThumbnailImgFmts } from '$lib/types';
+import type { ArticleMetadata } from '$lib/types';
 
 export const load: PageLoad = async ({
 	fetch
-}): Promise<{
-	articles: ArticleMetadata[];
-	thumbnailImgFmts: ArticleThumbnailImgFmts;
-}> => {
+}): Promise<{ articles: ArticleMetadata[]; }> => {
 	const articles = await (await fetch('/api/articles?indexed=true&l=4')).json();
-	const thumbnailImgFmts = await (await fetch('/api/articles/thumbnail-imgs')).json();
-	return { articles, thumbnailImgFmts };
+	return { articles };
 };
