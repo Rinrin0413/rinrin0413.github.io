28e8863855221783ef0946f3ab1cc46da280aa08
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-11-10 02:53:10 +0900

    🎨 Style: format code

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,7 @@
 import type { PageLoad } from './$types';
 import type { ArticleMetadata } from '$lib/types';
 
-export const load: PageLoad = async ({
-	fetch
-}): Promise<{ articles: ArticleMetadata[]; }> => {
+export const load: PageLoad = async ({ fetch }): Promise<{ articles: ArticleMetadata[] }> => {
 	const articles = await (await fetch('/api/articles?indexed=true&l=4')).json();
 	return { articles };
 };
