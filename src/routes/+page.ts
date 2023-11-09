e801b51989786073ba108239cc3b4d84558da43a
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-11-10 02:57:35 +0900

    🛠️♻️ update article limit of top page [#23]
    - 🛠️ Update: article limit of top page from 4 to 3
    - ♻️ Add: article limit own constant `ARTICLE_LIMIT`

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,6 +2,7 @@ import type { PageLoad } from './$types';
 import type { ArticleMetadata } from '$lib/types';
 
 export const load: PageLoad = async ({ fetch }): Promise<{ articles: ArticleMetadata[] }> => {
-	const articles = await (await fetch('/api/articles?indexed=true&l=4')).json();
+	const ARTICLE_LIMIT = 3;
+	const articles = await (await fetch('/api/articles?indexed=true&l=' + ARTICLE_LIMIT)).json();
 	return { articles };
 };
