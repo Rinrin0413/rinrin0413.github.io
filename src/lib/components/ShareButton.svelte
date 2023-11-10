e978b7990f381fd6b0b4ed10b8656c56feec17bb
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-11-11 00:50:56 +0900

    ✨ Impl: share button design [#25]

--- src/lib/components/ShareButton.svelte
+++ src/lib/components/ShareButton.svelte
@@ -36,3 +36,34 @@
     -->
 	<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M720-80q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322-392q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638-672L356-508q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Z"/></svg>
 </button>
+
+<style lang="scss">
+	@use '/assets/stylesheets/variables/color' as *;
+
+	button {
+		display: inline-block;
+		$size: 32px;
+		width: $size;
+		height: $size;
+		padding: 4px;
+		margin: 0 8px;
+		background: $button-primary;
+		border: 3px solid $button-secondary;
+		border-radius: 4px;
+		box-shadow: 0 0 14px #00000050;
+		box-sizing: content-box;
+		cursor: pointer;
+		transition: 0.2s;
+
+		&:hover {
+			filter: brightness(1.06);
+			transform: translateY(3px);
+		}
+	}
+
+	svg {
+		width: 100%;
+		height: 100%;
+		fill: $text-dark;
+	}
+</style>
