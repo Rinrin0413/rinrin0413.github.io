fe30e66304c73e7f877abd0fe30a7ae95334f56d
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-11-11 00:39:42 +0900

    🛠️ Improve: use SVG icon for share button [#25]

--- src/lib/components/ShareButton.svelte
+++ src/lib/components/ShareButton.svelte
@@ -27,4 +27,12 @@
 	}
 </script>
 
-<button on:click={share} title={$_('w.share')}>Share</button>
+<button on:click={share} title={$_('w.share')}>
+	<!-- 
+        Google Material Symbols and Icons - Share
+        https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Ashare%3AFILL%401%3Bwght%40400%3BGRAD%400%3Bopsz%4024
+        
+        This icon is licensed under the Apache License Version 2.0: https://github.com/google/material-design-icons/blob/master/README.md
+    -->
+	<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M720-80q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322-392q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638-672L356-508q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Z"/></svg>
+</button>
