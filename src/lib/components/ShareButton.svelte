5bbed46e9f1346acc44e0439b0c5cd42dd6f43f1
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-11-11 01:47:41 +0900

    🎨 Style: format code

--- src/lib/components/ShareButton.svelte
+++ src/lib/components/ShareButton.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import { SITE_NAME } from "$lib/variables";
-	import { browser } from "$app/environment";
-	import { _ } from "svelte-i18n";
+	import { SITE_NAME } from '$lib/variables';
+	import { browser } from '$app/environment';
+	import { _ } from 'svelte-i18n';
 
 	let isMenuOpened = false;
 
@@ -35,7 +35,11 @@
         
         This icon is licensed under the Apache License Version 2.0: https://github.com/google/material-design-icons/blob/master/README.md
     -->
-	<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M720-80q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322-392q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638-672L356-508q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Z"/></svg>
+	<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"
+		><path
+			d="M720-80q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322-392q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638-672L356-508q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Z"
+		/></svg
+	>
 </button>
 
 {#if isMenuOpened}
