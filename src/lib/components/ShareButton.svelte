543328a7a666369566c51a5c54111ef64fb8f753
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-11-12 03:29:20 +0900

    🎨 Style: format code

--- src/lib/components/ShareButton.svelte
+++ src/lib/components/ShareButton.svelte
@@ -28,7 +28,8 @@
 	function share() {
 		if (!browser) return;
 
-		if (navigator.share === undefined || true) { // DEBUG: ` || true`
+		// DEBUG: ` || true`
+		if (navigator.share === undefined || true) {
 			shareWithModal();
 		} else {
 			shareWithWebShareApi();
@@ -60,7 +61,12 @@
         
         This icon is licensed under the Apache License Version 2.0: https://github.com/google/material-design-icons/blob/master/README.md
     -->
-	<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" class="share-icn"
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		height="24"
+		viewBox="0 -960 960 960"
+		width="24"
+		class="share-icn"
 		><path
 			d="M720-80q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322-392q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638-672L356-508q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Z"
 		/></svg
@@ -85,10 +91,12 @@
 				{$_('blog.copyUrl')}
 			</button>
 		</li>
-		<li><button on:click={shareOnTwitter}>
-			<img src="/images/logos/x_logo.svg" alt="X logo">
-			{$_('blog.post')}
-		</button></li>
+		<li>
+			<button on:click={shareOnTwitter}>
+				<img src="/images/logos/x_logo.svg" alt="X logo" />
+				{$_('blog.post')}
+			</button>
+		</li>
 		<li><button>mastodon</button></li>
 		<li><button>Misskey</button></li>
 		<li><button>LINE</button></li>
@@ -159,7 +167,8 @@
 			}
 		}
 
-		svg, img {
+		svg,
+		img {
 			vertical-align: middle;
 		}
 
