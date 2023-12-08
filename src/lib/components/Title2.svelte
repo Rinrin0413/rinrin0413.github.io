64a64334701a20eba78d10cbee9dfc7a6bebb07b
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-12-08 18:32:05 +0900

    ✨ Impl: property `style` for component `Title2` [#12]

--- src/lib/components/Title2.svelte
+++ src/lib/components/Title2.svelte
@@ -6,9 +6,12 @@
 	 * Its purpose is to identify the element when linking (using a fragment identifier).
 	 */
 	export let id: string | undefined | null = null;
+
+	/** Defines inline CSS styles for an element. */
+	export let style: string | undefined | null = null;
 </script>
 
-<h2 {id}>{text}</h2>
+<h2 {id} {style}>{text}</h2>
 
 <style lang="scss">
 	@use '/assets/stylesheets/variables/color' as *;
