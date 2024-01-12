0744575ae6b41a303f458739c152b86f6aa1b074
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-01-13 01:45:40 +0900

    🎨 Update: ignore warn `a11y-no-noninteractive-tabindex` [#36]

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import preprocess from 'svelte-preprocess';
 import mdsvex from './mdsvex.config.js';
 
+function onWarn(warning, handler) {
+	const IGNORED_WARNS = ['a11y-no-noninteractive-tabindex'];
+	if (IGNORED_WARNS.includes(warning.code)) return;
+	handler(warning);
+
+	// console.log(warning.code);
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -23,7 +31,9 @@ const config = {
 		}
 	},
 
-	extensions: ['.svelte', '.md']
+	extensions: ['.svelte', '.md'],
+
+	onwarn: onWarn
 };
 
 export default config;
