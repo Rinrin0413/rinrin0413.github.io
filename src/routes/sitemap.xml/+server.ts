f815787b44adb5649675d2e463774a9fb8564b3d
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-05-30 02:36:43 +0900

    ✨ Add: sitemap [#51]

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,62 @@
+import type { RequestHandler } from './$types';
+import { fetchArticles } from '$lib/fetchers';
+import { SITE_URL } from '$lib/variables';
+
+export const prerender = true;
+
+export const GET: RequestHandler = async ({ setHeaders }) => {
+	setHeaders({
+		'Cache-Control': 'max-age=0, s-maxage=86400',
+		'Content-Type': 'application/xml; charset=utf-8',
+	});
+	return new Response(await body());
+};
+
+async function body() {
+	const articles = (await fetchArticles({ isOnlyIndexed: true }))
+		.map((article) => `<url>
+        <loc>${SITE_URL}/blog/articles/${article.slug}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>`).join('\n    ');
+
+	return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>${SITE_URL}</loc>
+        <changefreq>monthly</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>${SITE_URL}/profile</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>${SITE_URL}/blog</loc>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+	${articles}
+    <url>
+        <loc>${SITE_URL}/tools</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>${SITE_URL}/projects</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.5</priority>
+    </url>
+    <url>
+        <loc>${SITE_URL}/creations</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>
+    <url>
+        <loc>${SITE_URL}/social</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+</urlset>`;
+}
