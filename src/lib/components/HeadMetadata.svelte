c4fae5ead91078b19fb51940fbb6cda40f436356
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-06-12 19:12:20 +0900

    ✨ Impl: `HeadMetadata` component to manage Metadata

---
+++ src/lib/components/HeadMetadata.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	/** ex: `Home` */
+	export let title: string;
+	/** ex: `Rinrin.rs のホームページです。` */
+	export let desc: string;
+	/** ex: `{SITE_URL}/profile` */
+	export let canonicalUrl: string;
+
+	$: fullTitle = 'Rinrin.rs | ' + title;
+</script>
+
+<svelte:head>
+	<title>{fullTitle}</title>
+	<meta name="title" content={fullTitle} />
+	<meta name="description" content={desc} />
+
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={desc} />
+	<meta property="og:url" content={canonicalUrl} />
+
+	<link rel="canonical" href={canonicalUrl} />
+</svelte:head>
