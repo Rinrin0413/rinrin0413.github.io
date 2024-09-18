f89f0aa106cef9c2b0be6da19de5bf786f93e5af
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-09-19 02:55:09 +0900

    🛠️ Change: background wallpaper

--- src/lib/scripts/data/bg-img-url.ts
+++ src/lib/scripts/data/bg-img-url.ts
@@ -1,2 +1,2 @@
 /** The Aipictors URL of the background wallpaper image */
-export const BG_IMG_URL: string | null = 'https://aipictors.com/works/396129';
+export const BG_IMG_URL: string | null = 'https://aipictors.com/works/460630/';
