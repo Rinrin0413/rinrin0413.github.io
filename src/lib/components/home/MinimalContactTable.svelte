d99b3a3b73e852ee41250b872279651d60a17a0d
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-10-06 19:48:18 +0900

    🎨 Style: format code

--- src/lib/components/home/MinimalContactTable.svelte
+++ src/lib/components/home/MinimalContactTable.svelte
@@ -5,13 +5,18 @@
 <table class="social-table">
 	<tr>
 		<td>
-			<img src="/images/logos/gmail.webp" alt="Gmail logo" width=39 height=39 />Gmail
+			<img src="/images/logos/gmail.webp" alt="Gmail logo" width="39" height="39" />Gmail
 		</td>
 		<td><EaButton mini /></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="/images/logos/discord-mark-blue.svg" alt="Discord logo" width=26 height=20 />Discord
+			<img
+				src="/images/logos/discord-mark-blue.svg"
+				alt="Discord logo"
+				width="26"
+				height="20"
+			/>Discord
 		</td>
 		<td
 			><a
@@ -23,7 +28,12 @@
 	</tr>
 	<tr>
 		<td title="My Discord server">
-			<img src="/images/logos/discord-mark-blue.svg" alt="Discord logo" width=26 height=20 />Discord(server)
+			<img
+				src="/images/logos/discord-mark-blue.svg"
+				alt="Discord logo"
+				width="26"
+				height="20"
+			/>Discord(server)
 		</td>
 		<td
 			><a href="https://discord.gg/7QhMDfyPHR" target="_blank" rel="noopener noreferrer"
@@ -41,7 +51,7 @@
 	</tr>
 	<tr>
 		<td>
-			<img src="/images/logos/github-mark.svg" alt="GitHub logo" width=98 height=96 />GitHub
+			<img src="/images/logos/github-mark.svg" alt="GitHub logo" width="98" height="96" />GitHub
 		</td>
 		<td>
 			<a href="https://github.com/Rinrin0413" target="_blank" rel="noopener noreferrer"
@@ -51,7 +61,7 @@
 	</tr>
 	<tr>
 		<td>
-			<img src="/images/logos/x_logo.svg" alt="X logo" width=1200 height=1227 />X
+			<img src="/images/logos/x_logo.svg" alt="X logo" width="1200" height="1227" />X
 		</td>
 		<td
 			><a href="https://x.com/Rinrin_2nd" target="_blank" rel="noopener noreferrer">@Rinrin_2nd</a
