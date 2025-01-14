dffceeb904cd384a31f7991174ced1e8d151f2fe
Rinrin.rs <rinrin0413.valley@gmail.com>
2025-01-15 05:11:16 +0900

    ⚡ Improve: icon `Content Copy` is now file-based instead of inline `svg` element

--- src/lib/btpc/components/ShareButton.svelte
+++ src/lib/btpc/components/ShareButton.svelte
@@ -96,16 +96,12 @@
 	>
 		<li>
 			<button on:click={copyToClipboard} title={ITEM_NAMES.copy} bind:this={firstItem}>
-				<!--
-					Google Material Symbols and Icons - Content Copy
-					https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Acontent_copy%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024
-					This icon is licensed under the Apache License Version 2.0: https://github.com/google/material-design-icons/blob/master/README.md
-				-->
-				<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"
-					><path
-						d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"
-					/></svg
-				>
+				<img
+					src="/images/google-material-design-icons/content_copy_24dp_533618_FILL0_wght400_GRAD0_opsz24_trimmed.svg"
+					alt={$_('copy.copy')}
+					width=17
+					height=20
+				/>
 				{#if !expanded}
 					{ITEM_NAMES.copy}
 				{/if}
