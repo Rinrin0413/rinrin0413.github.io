Rinrin0413/rinrin0413.github.io

commit 9354e800377c923e6c78a75af9be5153aa506115

⚡ Improve: alternative texts for `img` tags
- 🛠️ Improve: some alternative texts are now more descriptive
- 🛠️ Change: unnecessary alternative texts are now empty strings

src/routes/blog/articles/[slug]/+page.svelte
@@ -39,7 +39,7 @@
 
 	$: thumbnailImgFmt = data.thumbnailImgFmt;
 	$: hasThumbnailImg = thumbnailImgFmt !== null;
-	$: thumbnailPath = hasThumbnailImg ? `/images/blog/thumbnails/${slug}.` + thumbnailImgFmt : null;
+	$: thumbnailPath = hasThumbnailImg ? `/images/blog/thaumbnails/${slug}.` + thumbnailImgFmt : null;
 
 	function introAnim(index = 0) {
 		return {
@@ -79,19 +79,14 @@
 {#if hasThumbnailImg}
 	<img
 		src={thumbnailPath}
-		alt={$_('w.backImage')}
+		alt=""
 		class="back-thumbnail"
 		style={parallax(0.25)}
 		aria-hidden="true"
 	/>
 
 	<div class="thumbnail-wrapper" in:scale|global={introAnim()}>
-		<img
-			src={thumbnailPath}
-			alt={$_('w.articleThumbnailImage')}
-			class="thumbnail"
-			style={parallax(-0.25)}
-		/>
+		<img src={thumbnailPath} alt="" class="thumbnail" style={parallax(-0.25)} />
 	</div>
 {:else}
 	<Space height="86px" />

src/routes/social/+page.svelte
@@ -27,7 +27,7 @@
 		{#each SOCIALS as { service, name, url, icon }}
 			<tr>
 				<td>
-					{#if icon}<img src="/images/logos/{icon}" alt="{service} logo" />{/if}{service}
+					{#if icon}<img src="/images/logos/{icon}" alt="" />{/if}{service}
 				</td>
 				<td>
 					{#if url}

src/lib/btpc/components/ShareButton.svelte
@@ -98,7 +98,7 @@
 			<button on:click={copyToClipboard} title={ITEM_NAMES.copy} bind:this={firstItem}>
 				<img
 					src="/images/google-material-design-icons/content_copy_24dp_533618_FILL0_wght400_GRAD0_opsz24_trimmed.svg"
-					alt={$_('copy.copy')}
+					alt=""
 					width=17
 					height=20
 				/>
@@ -109,7 +109,7 @@
 		></li
 		><li>
 			<button on:click={shareOnTwitter} title={ITEM_NAMES.post}>
-				<img src="/images/logos/x_logo.svg" alt="X logo" width=1200 height=1227 />
+				<img src="/images/logos/x_logo.svg" alt="" width=1200 height=1227 />
 				{#if !expanded}
 					{ITEM_NAMES.post}
 				{/if}
@@ -117,7 +117,7 @@
 		></li
 		><li>
 			<button on:click={shareOnMisskey} title={ITEM_NAMES.note}>
-				<img src="/images/logos/misskey_icon.webp" alt="Misskey logo" width=92 height=64 />
+				<img src="/images/logos/misskey_icon.webp" alt="" width=92 height=64 />
 				{#if !expanded}
 					{ITEM_NAMES.note}
 				{/if}
@@ -126,7 +126,7 @@
 		>{#if !expanded}
 			<li>
 				<button class:opened={false} on:click={shareOnMastodon} title={ITEM_NAMES.toot}>
-					<img src="/images/logos/mastodon_logo-purple.svg" alt="Mastodon logo" width=75 height=79 />
+					<img src="/images/logos/mastodon_logo-purple.svg" alt="" width=75 height=79 />
 					{ITEM_NAMES.toot}
 				</button>
 				<form on:submit={shareOnMastodon}>

src/lib/components/CopyButton.svelte
@@ -26,7 +26,7 @@
 >
 	<img
 		src="/images/google-material-design-icons/content_copy_24dp_533618_FILL0_wght400_GRAD0_opsz24_trimmed.svg"
-		alt={$_('copy.copy')}
+		alt="Copy"
 	/>
 </button>
 

src/lib/components/blog/ArticleCard.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import type { ArticleMetadata, ArticleThumbnailImgFmts } from '$lib/btpc/scripts/types';
 	import { idToDate } from '$lib/scripts/utils';
-	import { _ } from 'svelte-i18n';
 	import { date as dateI18n } from 'svelte-i18n';
 
 	export let meta: ArticleMetadata;
@@ -31,7 +30,7 @@
 				src="/images/{hasThumbnailImg
 					? `blog/thumbnails/${slug}.${thumbnailImgFmt}`
 					: 'no-image_oxipng.png'}"
-				alt={$_('w.' + (hasThumbnailImg ? 'articleThumbnailImage' : 'noImage'))}
+				alt=""
 				loading="lazy"
 				class:no-image={!hasThumbnailImg}
 			/>

src/lib/components/header/Header.svelte
@@ -95,7 +95,7 @@
 		on:click={() => {
 			isDrawerMenuOpened.set(false);
 		}}
-		tabindex="-1"><img src="/images/logos/rinrin/logo.svg" alt={$_('header.logo')} /></a
+		tabindex="-1"><img src="/images/logos/rinrin/logo.svg" alt="" /></a
 	>
 </header>
 

src/lib/components/home/MinimalContactTable.svelte
@@ -5,18 +5,13 @@
 <table class="social-table">
 	<tr>
 		<td>
-			<img src="/images/logos/gmail.webp" alt="Gmail logo" width="39" height="39" />Gmail
+			<img src="/images/logos/gmail.webp" alt="" width="39" height="39" />Gmail
 		</td>
 		<td><EaButton mini /></td>
 	</tr>
 	<tr>
 		<td>
-			<img
-				src="/images/logos/discord-mark-blue.svg"
-				alt="Discord logo"
-				width="26"
-				height="20"
-			/>Discord
+			<img src="/images/logos/discord-mark-blue.svg" alt="" width="26" height="20" />Discord
 		</td>
 		<td
 			><a
@@ -28,12 +23,7 @@
 	</tr>
 	<tr>
 		<td title="My Discord server">
-			<img
-				src="/images/logos/discord-mark-blue.svg"
-				alt="Discord logo"
-				width="26"
-				height="20"
-			/>Discord(server)
+			<img src="/images/logos/discord-mark-blue.svg" alt="" width="26" height="20" />Discord(server)
 		</td>
 		<td
 			><a href="https://discord.gg/7QhMDfyPHR" target="_blank" rel="noopener noreferrer"
@@ -43,12 +33,7 @@
 	</tr>
 	<tr>
 		<td>
-			<img
-				src="/images/logos/misskey-io_io-icon.svg"
-				alt="Misskey.io logo"
-				width="26"
-				height="26"
-			/>Misskey.io
+			<img src="/images/logos/misskey-io_io-icon.svg" alt="" width="26" height="26" />Misskey.io
 		</td>
 		<td
 			><a href="https://misskey.io/@Rinrin0413" target="_blank" rel="noopener noreferrer"
@@ -58,7 +43,7 @@
 	</tr>
 	<tr>
 		<td>
-			<img src="/images/logos/github-mark.svg" alt="GitHub logo" width="98" height="96" />GitHub
+			<img src="/images/logos/github-mark.svg" alt="" width="98" height="96" />GitHub
 		</td>
 		<td>
 			<a href="https://github.com/Rinrin0413" target="_blank" rel="noopener noreferrer"
@@ -68,7 +53,7 @@
 	</tr>
 	<tr>
 		<td>
-			<img src="/images/logos/x_logo.svg" alt="X logo" width="1200" height="1227" />X
+			<img src="/images/logos/x_logo.svg" alt="" width="1200" height="1227" />X
 		</td>
 		<td
 			><a href="https://x.com/Rinrin_2nd" target="_blank" rel="noopener noreferrer">@Rinrin_2nd</a

src/routes/+error.svelte
@@ -26,11 +26,11 @@
 		{$_('error.pageNotFound.0')}<br />
 		{$_('error.pageNotFound.1')}
 	</p>
-	<img src="/images/404.webp" alt="404 Not like this" class="e404" />
+	<img src="/images/404.webp" alt="" class="e404" />
 {:else}
 	<Title text="Unexpected error" />
 	<p>{msg}</p>
-	<img src="https://http.cat/{status}.jpg" alt=" " loading="lazy" />
+	<img src="https://http.cat/{status}.jpg" alt="" loading="lazy" />
 {/if}
 
 <style lang="scss">

commit de776559653a41e3af27200869c621fdc372a87d

🐛 Fix: thumbnail images were not being displaying on article pages
Caused by the commit 9354e800377c923e6c78a75af9be5153aa506115.

src/routes/blog/articles/[slug]/+page.svelte
@@ -39,7 +39,7 @@
 
 	$: thumbnailImgFmt = data.thumbnailImgFmt;
 	$: hasThumbnailImg = thumbnailImgFmt !== null;
-	$: thumbnailPath = hasThumbnailImg ? `/images/blog/thaumbnails/${slug}.` + thumbnailImgFmt : null;
+	$: thumbnailPath = hasThumbnailImg ? `/images/blog/thumbnails/${slug}.` + thumbnailImgFmt : null;
 
 	function introAnim(index = 0) {
 		return {