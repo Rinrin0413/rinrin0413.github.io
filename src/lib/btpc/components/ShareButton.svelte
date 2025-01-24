941c15e401b7f7d9eaec3f8db15c84411e235569
Rinrin.rs <rinrin0413.valley@gmail.com>
2025-01-24 15:23:54 +0900

    ✨ Add: alternative texts for expanded share button items

--- src/lib/btpc/components/ShareButton.svelte
+++ src/lib/btpc/components/ShareButton.svelte
@@ -106,7 +106,7 @@
 			<button on:click={copyToClipboard} title={ITEM_NAMES.copy} bind:this={firstItem}>
 				<img
 					src="/images/google-material-design-icons/content_copy_24dp_533618_FILL0_wght400_GRAD0_opsz24_trimmed.svg"
-					alt=""
+					alt={expanded ? ITEM_NAMES.copy : ''}
 					width=17
 					height=20
 				/>
@@ -117,7 +117,7 @@
 		></li
 		><li>
 			<button on:click={shareOnTwitter} title={ITEM_NAMES.post}>
-				<img src="/images/logos/x_logo.svg" alt="" width=1200 height=1227 />
+				<img src="/images/logos/x_logo.svg" alt={expanded ? ITEM_NAMES.post : ''} width=1200 height=1227 />
 				{#if !expanded}
 					{ITEM_NAMES.post}
 				{/if}
@@ -125,7 +125,7 @@
 		></li
 		><li>
 			<button on:click={shareOnMisskey} title={ITEM_NAMES.note}>
-				<img src="/images/logos/misskey_icon.webp" alt="" width=92 height=64 />
+				<img src="/images/logos/misskey_icon.webp" alt={expanded ? ITEM_NAMES.note : ''} width=92 height=64 />
 				{#if !expanded}
 					{ITEM_NAMES.note}
 				{/if}
