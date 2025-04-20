91c40ebe71226823da405214d2534085e538d421
Rinrin.rs <rinrin0413.valley@gmail.com>
2025-04-21 04:57:28 +0900

    ✨ Feat: add graph to web tool `awake2sleep-inferencer` [#131]

--- src/routes/tools/awake2sleep-inferencer/+page.svelte
+++ src/routes/tools/awake2sleep-inferencer/+page.svelte
@@ -9,9 +9,14 @@
 <script lang="ts">
 	import ToolHead from '$lib/components/tools/ToolHead.svelte';
 	import CopyButton from '$lib/components/CopyButton.svelte';
+	import { Line } from 'svelte-chartjs';
 	import Hr from '$lib/components/Hr.svelte';
 	import Title2 from '$lib/components/Title2.svelte';
 	import ToolFooter from '$lib/components/tools/ToolFoot.svelte';
+
+	import type { ChartOptions, ChartData, Point } from 'chart.js';
+	import { Chart, registerables } from 'chart.js';
+	import { browser } from '$app/environment';
 
 	// prettier-ignore
 	const DATASET: {
@@ -25,15 +30,67 @@
 		regrModels: { name: string; f: (x: number) => number; r2: number }[];
 	} = {sampleCount:311,date:'2025-04-20',stats:{awake:{mean:17.482636655987136,stdDeviation:5.576668342230824},sleep:{mean:10.066345123331184,stdDeviation:3.437263108110947},correlation:0.18526524472012193},regrModels:[{name:'4次多項式回帰',f:(x)=>6.0007161361915875+1.1261044683081043*x-0.11368375752642701*x**2+0.004578091965373962*x**3-0.000060369131017537305*x**4,r2:0.044181544796635275},{name:'2次多項式回帰',f:(x)=>9.602040463308338-0.06412604582751077*x+0.004709447670086122*x**2,r2:0.03881088048317516},{name:'指数回帰',f:(x)=>7.449187278567423*Math.exp(0.01343547554986219*x),r2:0.038178821761696846},{name:'線形回帰',f:(x)=>0.11419101008199448*x+8.069985184687518,r2:0.03432321090120094},{name:'累乗回帰',f:(x)=>5.7079439013207445*x**0.17886313176867122,r2:0.03142987489910054},{name:'対数回帰',f:(x)=>1.4546280168038384*Math.log(x)+5.99080998835628,r2:0.02587095921249649}]};
 
+	const MAX_AWAKE_DURATION = 35;
+
+	const CHART_OPTIONS: ChartOptions<'line'> = {
+		scales: {
+			x: { title: { display: true, text: '覚醒時間 (時間)' }, min: 0, max: MAX_AWAKE_DURATION },
+			y: { title: { display: true, text: '睡眠時間 (時間)' }, min: 5, max: 14 }
+		}
+	};
+
+	let renderGraph = false;
+	let chartWidth = 7;
+
+	if (renderGraph && browser) chartWidth = window.innerWidth < 700 ? 4 : 7;
+
+	Chart.register(...registerables);
+
 	let awakeDuration = parseFloat(DATASET.stats.awake.mean.toFixed(1));
 
 	$: sleepDurations = DATASET.regrModels.reduce(
 		(acc, model) => {
-			acc[model.name] = model.f(awakeDuration).toString();
+			acc[model.name] = model.f(awakeDuration);
 			return acc;
 		},
-		{} as Record<string, string>
+		{} as Record<string, number>
 	);
+
+	let chartData: ChartData<'line', (number | Point)[], unknown>;
+	$: if (renderGraph && chartData !== undefined)
+		chartData.datasets[DATASET.regrModels.length].data[0] = {
+			x: awakeDuration,
+			y:
+				DATASET.regrModels.reduce((acc, model) => {
+					let sleepDuration = sleepDurations[model.name];
+					if (sleepDuration < 0) sleepDuration = 0;
+					return acc + sleepDuration;
+				}, 0) / DATASET.regrModels.length
+		};
+
+	function setChartData() {
+		if (!renderGraph || chartData !== undefined) return;
+		const xLabels = Array.from({ length: MAX_AWAKE_DURATION * 2 + 1 }, (_, i) => i * 0.5);
+		chartData = {
+			labels: xLabels,
+			datasets: [
+				...DATASET.regrModels.map((model) => ({
+					label: model.name,
+					data: xLabels.map((x) => model.f(x))
+				})),
+				{
+					label: '推論値平均',
+					data: [],
+					pointBackgroundColor: '#38220b',
+					pointBorderWidth: 0,
+					pointRadius: 8,
+					pointHoverRadius: 16,
+					showLine: false,
+					animation: false
+				}
+			]
+		};
+	}
 </script>
 
 <ToolHead {metadata} />
@@ -45,7 +102,7 @@
 			type="number"
 			bind:value={awakeDuration}
 			min="0"
-			max="35"
+			max={MAX_AWAKE_DURATION}
 			step="0.5"
 			id="awake-dur"
 		/><label for="awake-dur">時間</label>
@@ -64,6 +121,19 @@
 				<li><CopyButton text={duration} /><span>{duration}</span>時間</li>
 			{/each}
 		</ul>
+		{#if renderGraph}
+			<div class="chart card">
+				<Line data={chartData} options={CHART_OPTIONS} width={chartWidth} height={4} />
+			</div>
+		{/if}
+		<p>
+			<label for="render-graph">グラフを表示</label><input
+				type="checkbox"
+				bind:checked={renderGraph}
+				on:change={setChartData}
+				id="render-graph"
+			/>
+		</p>
 	</div>
 	<Hr />
 	<div class="result">
@@ -98,8 +168,15 @@
 
 	@use '$lib/stylesheets/variables/mixin' as *;
 
-	input {
+	input[type='number'] {
 		width: 80px;
+	}
+
+	.chart {
+		max-width: min(1028px, 90%);
+		margin-top: 22px !important;
+		background-color: #ffffffb0 !important;
+		border-color: #ffffff !important;
 	}
 
 	.dataset,
