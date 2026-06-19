658bd861bbf17c9fd49a6f9bfe2cf136da28b01d
Rinrin.rs <rinrin0413.rs@gmail.com>
2026-06-19 12:50:33 +0900

    ♻️ Refactor: simplify dynamic component rendering [#179 #180]
    Destructure the 'component' prop directly to 'ChildComponent'. This capitalizes the variable name, allowing it to be used as a Svelte component tag directly and eliminating the need for the reactive '$derived' wrapper declaration.

--- src/lib/btpc/components/ChildPageComponentRenderer.svelte
+++ src/lib/btpc/components/ChildPageComponentRenderer.svelte
@@ -5,12 +5,10 @@
 		component: Component;
 	}
 
-	let { component }: Props = $props();
-
-	const SvelteComponent_1 = $derived(component);
+	let { component: ChildComponent }: Props = $props();
 </script>
 
-<article class="child-page-body"><div><SvelteComponent_1 /></div></article>
+<article class="child-page-body"><div><ChildComponent /></div></article>
 
 <style lang="scss" global>
 	@use '$lib/btpc/stylesheets/for_markdown';
